e3b2c7f5000d508b72c602b6dd0d15d674696f8f
mbknust <maks@knust.one>
2019-10-13 15:58:04 +0200

    I don't know how to name this commit

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def myActivity():
                     return Response("", status=500, mimetype='application/json')
                 insertactivity(session['username'], time, dist, sport)
                 return redirect(url_for('myActivity'))
-        return render_template('myActivity.html', username = session.get('username'))
+        return render_template('myActivity.html', username = session.get('username'), selectbest=sb)
     else:
         return redirect(url_for('main'))
 
